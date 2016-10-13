61f72adca3ad5b741d5c6c9b08c1486c174c48e0
Nikolay Kovalenko <nkovalenko@smedialink.com>
2016-10-13 10:37:40 +0700

    Globals settings added

---
+++ src/app/globals.js
@@ -0,0 +1,3 @@
+'use strict';
+exports.backendServerUrl = "http://127.0.0.1:8000";
+//# sourceMappingURL=globals.js.map
---
+++ src/app/globals.ts
@@ -0,0 +1,3 @@
+'use strict';
+
+export var backendServerUrl: string="http://127.0.0.1:8000";
